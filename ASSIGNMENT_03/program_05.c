8f32957a9d80ffe98bdd58fe742055441e5a396a
Sumit-Shastri <shastrisumit25@gmail.com>
2025-11-02 18:18:03 +0530

    Assignment 3 - program 5

---
+++ ASSIGNMENT_03/program_05.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+ 
+typedef int BOOL;
+#define TRUE 1
+#define FALSE 0
+
+BOOL CheckVowel(char ch)
+{
+    if(ch == 'a' ||
+       ch == 'e' ||
+       ch == 'i' ||
+       ch == 'o' ||
+       ch == 'u')
+    {
+        return TRUE;
+    }
+
+    else
+    {
+        return FALSE;
+    }
+}
+
+int main()
+{
+    char cValue = '\0';
+    BOOL bRet = FALSE;
+
+    printf("Enter Characteristic : \n");
+    scanf("%c",&cValue);
+
+    bRet = CheckVowel(cValue);
+
+    if(bRet == TRUE)
+    {
+        printf("It is a Vowel");
+    }
+
+    else
+    {
+        printf("It is not Vowel");
+    }
+
+    return 0;
+}
